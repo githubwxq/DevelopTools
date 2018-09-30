d954587d3194c8eeb9fd59b3a263b24e35387646
王晓清 <805380422@qq.com>
2018-09-30 15:36:03 +0800

    添加剪切板工具类

--- uitestdemo/src/main/module/java/com/example/uitestdemo/UIMainActivity.java
+++ uitestdemo/src/main/module/java/com/example/uitestdemo/UIMainActivity.java
@@ -1,5 +1,7 @@
 package com.example.uitestdemo;
 
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 import com.alibaba.android.arouter.launcher.ARouter;
 
 import com.juziwl.uilibrary.notification.NotificationUtils;
+import com.wxq.commonlibrary.util.ClipboardUtils;
 import com.wxq.commonlibrary.util.ToastUtils;
 import com.wxq.mvplibrary.base.BaseActivity;
 import com.wxq.mvplibrary.base.BasePresenter;
@@ -28,6 +31,10 @@ public class UIMainActivity extends BaseActivity {
 
     @BindView(R.id.tv_test)
     TextView tvTestUi;
+
+    @BindView(R.id.tv_test_clickManager)
+    TextView tv_test_clickManager;
+
 
     int a = 0;
 
@@ -59,9 +66,31 @@ public class UIMainActivity extends BaseActivity {
                 a++;
 //                 int a=0;
 //             boolean judgeNull=  user.accessToken.equals("1");
-                NotificationUtils.showCommonNofition(UIMainActivity.this,"id","name");
+                NotificationUtils.showCommonNofition(UIMainActivity.this, "id", "name");
+
+
+                //设置剪切板信息
+
+                ClipboardUtils.copyText(UIMainActivity.this,"wxwxqwxqwxqwxqwxqwxqwxqwxqwxqq");
+
             }
         });
+
+
+        tv_test_clickManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //点击 获取剪切板信息
+                ClipboardManager clipboardManager= (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+
+                CharSequence sequence = clipboardManager.getPrimaryClip().getItemAt(0).getText();
+
+                ToastUtils.showShort(sequence);
+
+            }
+        });
+
+
     }
 
     @Override
