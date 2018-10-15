ad84f50b68554ce7e6268f3c48922df70669583a
王网明 <wangwangming@juziwl.com>
2018-10-15 10:26:06 +0800

    更改gradle版本，升级buildTools版本和support版本 更换compile 依赖方式为 api

--- nettestdemo/src/main/module/java/com/example/news/MainActivity.java
+++ nettestdemo/src/main/module/java/com/example/news/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.nettestdemo;
+package com.example.news;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.nettestdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
