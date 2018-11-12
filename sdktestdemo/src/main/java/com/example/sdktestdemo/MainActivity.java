d49a7c3fc491b07aca03fa7f3e04910e9716d4f9
王晓清 <805380422@qq.com>
2018-11-12 19:54:22 +0800

    组件化优化处理

---
+++ sdktestdemo/src/main/java/com/example/sdktestdemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.sdktestdemo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.wxq.mvplibrary.base.BaseActivity;
+import com.wxq.mvplibrary.base.BasePresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+
+
+public class MainActivity extends BaseActivity {
+
+
+    @BindView(R2.id.tv_title)
+    TextView tvTitle;
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int attachLayoutRes() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected BasePresenter initPresent() {
+        return null;
+    }
+
+
+    @OnClick(R2.id.tv_title)
+    public void onViewClicked() {
+    }
+}
