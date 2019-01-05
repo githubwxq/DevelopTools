6ae5b334c0e2aa767c24159de6786249d9c4b394
王晓清 <805380422@qq.com>
2019-01-06 04:57:50 +0800

    bmo添加登入模块

---
+++ module_login/src/main/java/com/example/module_login/LoginModuleApplication.java
@@ -0,0 +1,31 @@
+package com.example.module_login;
+
+import com.wxq.mvplibrary.base.BaseApp;
+
+import cn.bmob.v3.Bmob;
+
+/**
+ * author:wxq
+ * email:805380422@qq.com
+ * time:2019/01/04
+ * desc: bmob项目
+ * version:1.0
+ */
+public class LoginModuleApplication extends BaseApp {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Bmob.initialize(this, "ea796f59c1a3d4a34f0b18b7626dd291");
+
+    }
+
+    @Override
+    public boolean setIsDebug() {
+        return false;
+    }
+
+    @Override
+    public void dealWithException(Thread thread, Throwable throwable, String error) {
+
+    }
+}
---
+++ module_login/src/main/java/com/example/module_login/activity/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.module_login.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.module_login.R;
+import com.wxq.mvplibrary.base.BaseActivity;
+import com.wxq.mvplibrary.base.BasePresenter;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int attachLayoutRes() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected BasePresenter initPresent() {
+        return null;
+    }
+}
