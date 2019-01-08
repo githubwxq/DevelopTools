b56e98c28ba0f6c99a0b9f85455331389bafba74
heqijun <578391255@qq.com>
2019-01-08 14:48:58 +0800

    --first commit--

---
+++ module_wanandroid/src/main/java/com/heqijun/module_wanandroid/app/PlayAndroidApp.kt
@@ -0,0 +1,18 @@
+package com.heqijun.module_wanandroid.app
+
+import com.heqijun.module_wanandroid.BuildConfig
+import com.wxq.mvplibrary.base.BaseApp
+
+/**
+ * Created by heqijun on 2019/1/8.
+ */
+class PlayAndroidApp : BaseApp() {
+
+    override fun dealWithException(thread: Thread?, throwable: Throwable?, error: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setIsDebug(): Boolean {
+        return BuildConfig.DEBUG
+    }
+}
