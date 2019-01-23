24d2299e914836c20ee6c8ae79b2c111d9bf5162
王晓清 <805380422@qq.com>
2019-01-24 02:26:11 +0800

    bmob聊天

---
+++ bmobim/src/main/java/com/example/bmobim/BmobImApplication.java
@@ -0,0 +1,85 @@
+package com.example.bmobim;
+
+import com.orhanobut.logger.Logger;
+import com.wxq.commonlibrary.base.BaseApp;
+import com.wxq.commonlibrary.util.BuglyUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import cn.bmob.newim.BmobIM;
+import cn.bmob.push.BmobPush;
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobInstallation;
+import cn.bmob.v3.BmobInstallationManager;
+import cn.bmob.v3.InstallationListener;
+import cn.bmob.v3.exception.BmobException;
+
+/**
+ * author:wxq
+ * email:805380422@qq.com
+ * time:2019/01/04
+ * desc: bmobim项目
+ * version:1.0
+ */
+public class BmobImApplication extends BaseApp {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Bmob.initialize(this, "ea796f59c1a3d4a34f0b18b7626dd291");
+        // 使用推送服务时的初始化操作
+        BmobInstallationManager.getInstance().initialize(new InstallationListener<BmobInstallation>() {
+            @Override
+            public void done(BmobInstallation bmobInstallation, BmobException e) {
+                if (e == null) {
+                    Logger.i(bmobInstallation.getObjectId() + "-" + bmobInstallation.getInstallationId());
+                } else {
+                    Logger.e(e.getMessage());
+                }
+            }
+        });
+        // 启动推送服务
+        BmobPush.startWork(this);
+        //初始化bugly
+        BuglyUtils.init(this,"bf24009ac5", BuildConfig.DEBUG);
+        //初始化im
+//        if (getApplicationInfo().packageName.equals(getMyProcessName())) {
+//            BmobIM.init(this);
+////            BmobIM.registerDefaultMessageHandler(new DemoMessageHandler(this));
+//        }
+
+
+
+    }
+
+    @Override
+    public boolean setIsDebug() {
+        return false;
+    }
+
+    @Override
+    public void dealWithException(Thread thread, Throwable throwable, String error) {
+
+    }
+
+
+
+    /**
+     * 获取当前运行的进程名
+     *
+     * @return
+     */
+    public static String getMyProcessName() {
+        try {
+            File file = new File("/proc/" + android.os.Process.myPid() + "/" + "cmdline");
+            BufferedReader mBufferedReader = new BufferedReader(new FileReader(file));
+            String processName = mBufferedReader.readLine().trim();
+            mBufferedReader.close();
+            return processName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ bmobim/src/main/java/com/example/bmobim/ImMainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bmobim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wxq.commonlibrary.base.BaseActivity;
+import com.wxq.commonlibrary.base.BasePresenter;
+
+public class ImMainActivity extends BaseActivity {
+
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int attachLayoutRes() {
+        return R.layout.activity_im_main;
+    }
+
+    @Override
+    protected BasePresenter initPresent() {
+        return null;
+    }
+}
