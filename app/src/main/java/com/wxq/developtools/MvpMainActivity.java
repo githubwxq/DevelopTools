e29a36a6804da768a278e47010a5018b4da23c7f
王晓清 <805380422@qq.com>
2019-02-19 13:20:01 +0800

    高德地图地址选择完成（搜索手动到时候加代码）

--- app/src/main/java/com/wxq/developtools/MvpMainActivity.java
+++ app/src/main/java/com/wxq/developtools/MvpMainActivity.java
@@ -9,6 +9,7 @@ import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.commonservice.ServiceCallBack;
 import com.example.commonservice.sdk.bean.SdkBean;
 import com.example.commonservice.sdk.service.SdkService;
+import com.juziwl.uilibrary.activity.ChooseMapAddressActivity;
 import com.juziwl.uilibrary.niceplayer.NiceVideoPlayer;
 import com.wxq.commonlibrary.util.ToastUtils;
 import com.wxq.commonlibrary.base.BaseActivity;
@@ -39,128 +40,128 @@ public class MvpMainActivity extends BaseActivity<MvpMainContract.Presenter> imp
 
     @Override
     protected void initViews() {
-
-
-        //配置
-
-
-//        tvHello.setText("1111111111111111111");
-
-
-        for (User user : DbManager.getInstance().getDaoSession().getUserDao().queryBuilder().list()) {
-
-//            com.orhanobut.logger.Logger.e("token", user.getAccessToken());
-        }
-
-        tvHello.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                mPresenter.getData(1);
-//                User user = new User();
-//                user.setUserName("wxq");
-//                user.setFlag(0);
-//                user.setAccessToken("token");
-//                DbManager.getInstance().getDaoSession().getUserDao().save(user);
-//
-//                int size = DbManager.getInstance().getDaoSession().getUserDao().queryBuilder().list().size();
-//
-//                showToast(size + "数据裤中数据");
-//
-//                Event event = new Event(2, "wxq");
-//
-//                RxBusManager.getInstance().post(event);
 //
 //
-//                SharedPreferencesUtils.setStringContent(MvpMainActivity.this,"wxqshare","name","wxq");
+//        //配置
 //
-
-
-//                try {
+//
+////        tvHello.setText("1111111111111111111");
+//
+//
+//        for (User user : DbManager.getInstance().getDaoSession().getUserDao().queryBuilder().list()) {
+//
+////            com.orhanobut.logger.Logger.e("token", user.getAccessToken());
+//        }
+//
+//        tvHello.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                mPresenter.getData(1);
+////                User user = new User();
+////                user.setUserName("wxq");
+////                user.setFlag(0);
+////                user.setAccessToken("token");
+////                DbManager.getInstance().getDaoSession().getUserDao().save(user);
+////
+////                int size = DbManager.getInstance().getDaoSession().getUserDao().queryBuilder().list().size();
+////
+////                showToast(size + "数据裤中数据");
+////
+////                Event event = new Event(2, "wxq");
+////
+////                RxBusManager.getInstance().post(event);
+////
+////
+////                SharedPreferencesUtils.setStringContent(MvpMainActivity.this,"wxqshare","name","wxq");
+////
+//
+//
+////                try {
+////                ARouter.getInstance()
+////                        .build(RouterContent.UI_MAIN)
+////                        .navigation();
+////                    startActivity(new Intent(MvpMainActivity.this,MainActivity.class));
+////                } catch (Exception e) {
+////                    e.printStackTrace();
+////                }
+//
+////                FileUtils.createOrExistsDir(GlobalContent.logPath);
+////
+////                FileUtils.appendToFile("wxqdsfasfasdfasd", GlobalContent.logPath+System.currentTimeMillis()+".text");
+//
+//                SdkService service = (SdkService) ARouter.getInstance().build(RouterContent.SDK_SERVICE).navigation();
+//                if (service!=null) {
+//                    //交互判断非空
+//                    service.getDataBack(new ServiceCallBack<String>() {
+//                        @Override
+//                        public void callBack(String result) {
+//                            ToastUtils.showShort(result);
+//                        }
+//                    });
+//                    SdkBean backBean = service.getSdkBean("11111");
+//                }
+//            }
+//        });
+//
+//
+//        tv_hello2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
 //                ARouter.getInstance()
-//                        .build(RouterContent.UI_MAIN)
+//                        .build(RouterContent.SDK_MAIN)
+//                        .withString("name", "name")
+//                        .withString("wxq", "wxq")
 //                        .navigation();
-//                    startActivity(new Intent(MvpMainActivity.this,MainActivity.class));
+//
+//
+//                try {
+////                    ARouter.getInstance().build(RouterContent.NETTEST_MAIN).navigation();
+////                    startActivity(new Intent(MvpMainActivity.this,MainActivity.class));
+//
+////                    、、下载
+//
+////                    DownloadHelper helper = new DownloadHelper("http://www.baidu.com", new DownloadListener() {
+////                        @Override
+////                        public void onStartDownload() {
+////
+////                        }
+////
+////                        @Override
+////                        public void onProgress(int progress) {
+////                            tvHello.setText(progress + "");
+////                        }
+////
+////                        @Override
+////                        public void onFinishDownload(File file) {
+////
+////                            tv_hello2.setText(file.getAbsolutePath());
+////                        }
+////
+////                        @Override
+////                        public void onFail(Throwable ex) {
+////
+////                        }
+////                    });
+////                    helper.downloadFile("https://dfsres-1254059237.cos.ap-shanghai.myqcloud.com/apppackage/testpad/ipadteacher.apk", GlobalContent.SAVEFILEPATH,"test.apk");
+//
+////                    try {
+////                        ARouter.getInstance()
+////                                .build(RouterContent.UI_MAIN)
+////                                .withString("name","arouter_main")
+////                                .withString("wxq","wxq")
+////                                .navigation();
+//////                    startActivity(new Intent(MvpMainActivity.this,MainActivity.class));
+////                    } catch (Exception e) {
+////                        e.printStackTrace();
+////                    }
+//
+//
 //                } catch (Exception e) {
 //                    e.printStackTrace();
 //                }
-
-//                FileUtils.createOrExistsDir(GlobalContent.logPath);
-//
-//                FileUtils.appendToFile("wxqdsfasfasdfasd", GlobalContent.logPath+System.currentTimeMillis()+".text");
-
-                SdkService service = (SdkService) ARouter.getInstance().build(RouterContent.SDK_SERVICE).navigation();
-                if (service!=null) {
-                    //交互判断非空
-                    service.getDataBack(new ServiceCallBack<String>() {
-                        @Override
-                        public void callBack(String result) {
-                            ToastUtils.showShort(result);
-                        }
-                    });
-                    SdkBean backBean = service.getSdkBean("11111");
-                }
-            }
-        });
-
-
-        tv_hello2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ARouter.getInstance()
-                        .build(RouterContent.SDK_MAIN)
-                        .withString("name", "name")
-                        .withString("wxq", "wxq")
-                        .navigation();
-
-
-                try {
-//                    ARouter.getInstance().build(RouterContent.NETTEST_MAIN).navigation();
-//                    startActivity(new Intent(MvpMainActivity.this,MainActivity.class));
-
-//                    、、下载
-
-//                    DownloadHelper helper = new DownloadHelper("http://www.baidu.com", new DownloadListener() {
-//                        @Override
-//                        public void onStartDownload() {
-//
-//                        }
-//
-//                        @Override
-//                        public void onProgress(int progress) {
-//                            tvHello.setText(progress + "");
-//                        }
-//
-//                        @Override
-//                        public void onFinishDownload(File file) {
-//
-//                            tv_hello2.setText(file.getAbsolutePath());
-//                        }
-//
-//                        @Override
-//                        public void onFail(Throwable ex) {
-//
-//                        }
-//                    });
-//                    helper.downloadFile("https://dfsres-1254059237.cos.ap-shanghai.myqcloud.com/apppackage/testpad/ipadteacher.apk", GlobalContent.SAVEFILEPATH,"test.apk");
-
-//                    try {
-//                        ARouter.getInstance()
-//                                .build(RouterContent.UI_MAIN)
-//                                .withString("name","arouter_main")
-//                                .withString("wxq","wxq")
-//                                .navigation();
-////                    startActivity(new Intent(MvpMainActivity.this,MainActivity.class));
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+//            }
+//        });
 
         //请求权限全部结果
         rxPermissions.request(
@@ -168,7 +169,9 @@ public class MvpMainActivity extends BaseActivity<MvpMainContract.Presenter> imp
                 Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        )
                 .subscribe(new Consumer<Boolean>() {
                     @Override
                     public void accept(Boolean granted) throws Exception {
@@ -177,6 +180,7 @@ public class MvpMainActivity extends BaseActivity<MvpMainContract.Presenter> imp
                         }
                         //不管是否获取全部权限，进入主页面
 //                        initCountDown();
+                        ChooseMapAddressActivity.navToActivity(MvpMainActivity.this);
                     }
                 });
 
--- uilibrary/src/main/java/com/juziwl/uilibrary/activity/ChooseMapAddressActivity.java
+++ uilibrary/src/main/java/com/juziwl/uilibrary/activity/ChooseMapAddressActivity.java
@@ -1,12 +1,21 @@
 package com.juziwl.uilibrary.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.ListView;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -22,17 +31,28 @@ import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.animation.Animation;
+import com.amap.api.maps.model.animation.TranslateAnimation;
 import com.amap.api.services.core.AMapException;
 import com.amap.api.services.core.LatLonPoint;
 import com.amap.api.services.core.PoiItem;
 import com.amap.api.services.geocoder.GeocodeResult;
 import com.amap.api.services.geocoder.GeocodeSearch;
 import com.amap.api.services.geocoder.RegeocodeAddress;
+import com.amap.api.services.geocoder.RegeocodeQuery;
 import com.amap.api.services.geocoder.RegeocodeResult;
+import com.amap.api.services.help.Inputtips;
+import com.amap.api.services.help.InputtipsQuery;
+import com.amap.api.services.help.Tip;
+import com.amap.api.services.poisearch.PoiResult;
 import com.amap.api.services.poisearch.PoiSearch;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.juziwl.uilibrary.R;
+import com.orhanobut.logger.Logger;
 import com.wxq.commonlibrary.base.BaseActivity;
 import com.wxq.commonlibrary.base.BasePresenter;
+import com.wxq.commonlibrary.util.StringUtils;
 import com.wxq.commonlibrary.util.ToastUtils;
 
 import java.util.ArrayList;
@@ -53,6 +73,15 @@ public class ChooseMapAddressActivity extends BaseActivity {
 
     private AMap aMap;
 
+
+    TabLayout tabLayout;
+
+    RecyclerView listView;
+    PoiItemAdapter adapter;
+
+    FloatingActionButton btn_done;
+
+    private String[] items = {"住宅区", "学校", "楼宇", "商场"};
     /**
      * 激活定位源监听
      */
@@ -93,8 +122,7 @@ public class ChooseMapAddressActivity extends BaseActivity {
                     firstItem.setProvinceName(regeocodeAddress.getProvince());
                     firstItem.setCityName(regeocodeAddress.getCity());
                     firstItem.setAdName(regeocodeAddress.getDistrict());
-
-//                    doSearchQuery();
+                    doSearchQuery();
                 }
             } else {
                 ToastUtils.showShort("error code is ");
@@ -106,16 +134,65 @@ public class ChooseMapAddressActivity extends BaseActivity {
 
         }
     };
+    private PoiSearch.OnPoiSearchListener onPoiSearchListener = new PoiSearch.OnPoiSearchListener() {
 
+        @Override
+        public void onPoiSearched(PoiResult poiResult, int resultCode) {
+            if (resultCode == AMapException.CODE_AMAP_SUCCESS) {
+                if (poiResult != null && poiResult.getQuery() != null) {
+                    if (poiResult.getQuery().equals(query)) {
+                        poiItems = poiResult.getPois();
+                        if (poiItems != null && poiItems.size() > 0) {
+                            resultData.clear();
+                            resultData.add(firstItem);
+                            resultData.addAll(poiItems);
+                            adapter.setSelectedPosition(0);
+                            adapter.notifyDataSetChanged();
+                        } else {
+                            ToastUtils.showShort("无搜索结果");
+                        }
+                    }
+                } else {
+                    ToastUtils.showShort("无搜索结果");
+                }
+            }
+        }
+
+        @Override
+        public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+        }
+    };
+
+
+
+
+
+
+
+
+    /**
+     * 开始进行poi搜索
+     */
+    protected void doSearchQuery() {
+        currentPage = 0;
+        query = new PoiSearch.Query(searchKey, searchType, "");// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query.setCityLimit(true);
+        query.setPageSize(20);
+        query.setPageNum(currentPage);
+
+        if (searchLatlonPoint != null) {
+            poiSearch = new PoiSearch(this, query);
+            poiSearch.setOnPoiSearchListener(onPoiSearchListener);
+            poiSearch.setBound(new PoiSearch.SearchBound(searchLatlonPoint, 1000, true));//
+            poiSearch.searchPOIAsyn();
+        }
+    }
 
     /**
      * 定图标记
      */
     private Marker locationMarker;
-
-    private String[] items = {"住宅区", "学校", "楼宇", "商场"};
-
-
 
     private List<PoiItem> resultData = new ArrayList<>();
 //    private SearchResultAdapter searchResultAdapter;
@@ -177,7 +254,7 @@ public class ChooseMapAddressActivity extends BaseActivity {
                     LatLng curLatlng = new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude());
                     aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLatlng, 16f));
                     searchLatlonPoint = new LatLonPoint(curLatlng.latitude, curLatlng.longitude);
-
+                    //获取定位的城市
                     city = amapLocation.getCity();
                 } else {
                     String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
@@ -203,8 +280,6 @@ public class ChooseMapAddressActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         mapView.onCreate(savedInstanceState);
         initMap();
-
-
     }
 
     private void initMap() {
@@ -212,7 +287,7 @@ public class ChooseMapAddressActivity extends BaseActivity {
             aMap = mapView.getMap();
             setUpMap();
         }
-// 在移动或者缩放地图的动作结束时，都会进 onCameraChangeFinish 回调中，获取此时的相机坐标作为 Marker 的坐标
+      // 在移动或者缩放地图的动作结束时，都会进 onCameraChangeFinish 回调中，获取此时的相机坐标作为 Marker 的坐标
         aMap.setOnCameraChangeListener(new AMap.OnCameraChangeListener() { // 在移动或者缩放地图的动作结束时，都会进 onCameraChangeFinish 回调中，获取此时的相机坐标作为 Marker 的坐标
             @Override
             public void onCameraChange(CameraPosition cameraPosition) {
@@ -222,8 +297,8 @@ public class ChooseMapAddressActivity extends BaseActivity {
             @Override
             public void onCameraChangeFinish(CameraPosition cameraPosition) {
                 if (!isItemClickAction && !isInputKeySearch) {
-//                    geoAddress();
-//                    startJumpAnimation();
+                    geoAddress();
+                    startJumpAnimation();
                 }
                 searchLatlonPoint = new LatLonPoint(cameraPosition.target.latitude, cameraPosition.target.longitude);
                 isInputKeySearch = false;
@@ -239,12 +314,62 @@ public class ChooseMapAddressActivity extends BaseActivity {
         });
 
 
+        geocoderSearch = new GeocodeSearch(this);
 
-
-//        geocoderSearch = new GeocodeSearch(this);
-//        geocoderSearch.setOnGeocodeSearchListener(onGeocodeSearchListener);
+//        、、searchlistener
+        geocoderSearch.setOnGeocodeSearchListener(onGeocodeSearchListener);
 
     }
+
+    /**
+     * 响应逆地理编码
+     */
+    public void geoAddress() {
+        RegeocodeQuery query = new RegeocodeQuery(searchLatlonPoint, 200, GeocodeSearch.AMAP);// 第一个参数表示一个Latlng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
+        geocoderSearch.getFromLocationAsyn(query);
+    }
+
+    /**
+     * 屏幕中心marker 跳动
+     */
+    public void startJumpAnimation() {
+        if (locationMarker != null) {
+            //根据屏幕距离计算需要移动的目标点
+            final LatLng latLng = locationMarker.getPosition();
+            Point point = aMap.getProjection().toScreenLocation(latLng);
+            point.y -= dip2px(this, 125);
+            LatLng target = aMap.getProjection()
+                    .fromScreenLocation(point);
+            //使用TranslateAnimation,填写一个需要移动的目标点
+            Animation animation = new TranslateAnimation(target);
+            animation.setInterpolator(new Interpolator() {
+                @Override
+                public float getInterpolation(float input) {
+                    // 模拟重加速度的interpolator
+                    if (input <= 0.5) {
+                        return (float) (0.5f - 2 * (0.5 - input) * (0.5 - input));
+                    } else {
+                        return (float) (0.5f - Math.sqrt((input - 0.5f) * (1.5f - input)));
+                    }
+                }
+            });
+            //整个移动所需要的时间
+            animation.setDuration(600);
+            //设置动画
+            locationMarker.setAnimation(animation);
+            //开始动画
+            locationMarker.startAnimation();
+        } else {
+            ToastUtils.showShort("screenMarker is null");
+        }
+    }
+
+    //dip和px转换
+    private static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
     private void addMarkerInScreenCenter(LatLng locationLatLng) {
         LatLng latLng = aMap.getCameraPosition().target;
         Point screenPosition = aMap.getProjection().toScreenLocation(latLng);
@@ -299,6 +424,71 @@ public class ChooseMapAddressActivity extends BaseActivity {
     @Override
     protected void initViews() {
         mapView=findViewById(R.id.map);
+
+        tabLayout=findViewById(R.id.tab_layout);
+        listView=findViewById(R.id.listview);
+        for (int i = 0; i < items.length; i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(items[i]));
+        }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                searchType = items[position];
+                geoAddress();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        listView.setLayoutManager(new LinearLayoutManager(this));
+        listView.setAdapter(adapter=new PoiItemAdapter(resultData));
+
+
+        btn_done=findViewById(R.id.btn_done);
+        btn_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                chooseFinish();
+                search("火车站", new Inputtips.InputtipsListener() {
+                    @Override
+                    public void onGetInputtips(List<Tip> list, int i) {
+                        ToastUtils.showShort(list.size()+"");
+                        LatLonPoint point = list.get(0).getPoint();
+                        String address= list.get(0).getAddress();
+                        searchPoi(list.get(0));
+                    }
+                });
+            }
+        });
+    }
+
+    private void chooseFinish() {
+        int pos = adapter.getSelectedPosition();
+        String area;
+        String addresss;
+        PoiItem poiItem = resultData.get(pos);
+        area = poiItem.getProvinceName() + poiItem.getCityName() + poiItem.getAdName();
+        if (pos == 0) {
+            addresss = poiItem.getTitle()
+                    .replace(poiItem.getProvinceName(), "")
+                    .replace(poiItem.getCityName(), "")
+                    .replace(poiItem.getAdName(), "");
+        } else {
+            addresss = poiItem.getSnippet() + poiItem.getTitle();
+        }
+        Intent data = new Intent();
+        data.putExtra("area", area);
+        data.putExtra("address", addresss);
+        setResult(RESULT_OK, data);
+        finish();
     }
 
     /**
@@ -351,4 +541,75 @@ public class ChooseMapAddressActivity extends BaseActivity {
         return null;
     }
 
+    private  class PoiItemAdapter extends BaseQuickAdapter<PoiItem,BaseViewHolder> {
+        private int selectedPosition = 0;
+        public int getSelectedPosition() {
+            return selectedPosition;
+        }
+
+        public void setSelectedPosition(int selectedPosition) {
+            this.selectedPosition = selectedPosition;
+        }
+
+
+        public PoiItemAdapter(List<PoiItem> resultData) {
+            super(R.layout.poiitem_item, resultData);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, PoiItem item) {
+            helper.setText(R.id.text_title,item.getTitle());
+            helper.setText(R.id.text_title_sub,item.getCityName()+item.getAdName()+item.getSnippet());
+            helper.setVisible(R.id.image_check,helper.getAdapterPosition() == selectedPosition ? true : false);
+
+//            imageCheck.setVisibility(helper.getAdapterPosition() == selectedPosition ? View.VISIBLE : View.INVISIBLE);
+
+            helper.getConvertView().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    LatLng curLatlng = new LatLng(item.getLatLonPoint().getLatitude(), item.getLatLonPoint().getLongitude());
+                    isItemClickAction = true;
+                    aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLatlng, 16f));
+                    setSelectedPosition(helper.getAdapterPosition());
+                    notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+//下面是搜索代码
+
+    private void search(String text,Inputtips.InputtipsListener inputtipsListener) {
+        InputtipsQuery inputquery = new InputtipsQuery(text, city);
+        Inputtips inputTips = new Inputtips(this, inputquery);
+        inputquery.setCityLimit(true);
+        inputTips.setInputtipsListener(inputtipsListener);
+        inputTips.requestInputtipsAsyn();
+    }
+
+    private String inputSearchKey;
+    public void searchPoi(Tip result) {
+        Logger.i("District: " + result.getDistrict() + ",Address: " + result.getAddress() + ",Name: " + result.getName());
+        isInputKeySearch = true;
+        inputSearchKey = result.getName();
+        searchLatlonPoint = result.getPoint();
+        firstItem = new PoiItem("tip", searchLatlonPoint, inputSearchKey, result.getAddress());
+        firstItem.setCityName(result.getDistrict());
+        firstItem.setAdName("");
+        resultData.clear();
+
+        adapter.setSelectedPosition(0);
+
+        aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(searchLatlonPoint.getLatitude(), searchLatlonPoint.getLongitude()), 16f));
+
+        doSearchQuery();
+    }
+
+
+
+
 }
+
+
+
+
