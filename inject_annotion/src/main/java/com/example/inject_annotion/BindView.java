c8d6abe2035dec077e22e4195d4f1b978f9950ab
王晓清 <805380422@qq.com>
2019-04-18 13:43:02 +0800

    移除service而外添加aspectj实验处理

--- inject_annotion/src/main/java/com/example/inject_annotion/BindView.java
+++ inject_annotion/src/main/java/com/example/inject_annotion/BindView.java
@@ -5,14 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * author:wxq
- * email:805380422@qq.com
- * time:2019/03/28
- * desc: 这个注解基于apt实现的
- * int id=element.getAnnotation(BindView.class).value();
- * version:1.0
- */
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.CLASS)
 public @interface BindView {
