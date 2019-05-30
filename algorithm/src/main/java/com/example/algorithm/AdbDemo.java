1c22bc6523d8e73fce3f66d9f44989153cec3d03
王晓清 <805380422@qq.com>
2019-05-30 23:37:12 +0800

    软引用

---
+++ algorithm/src/main/java/com/example/algorithm/AdbDemo.java
@@ -0,0 +1,59 @@
+package com.example.algorithm;
+
+import sun.applet.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class AdbDemo {
+
+    public static void main(String[] a){
+
+        Executors.newScheduledThreadPool(4).scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("wwwwww");
+                String adbHome="D:/develop_soft/androidsdk/platform-tools/";
+//                String cmd=adbHome+"adb version";  adbdevices
+                String cmd=adbHome+"adb -s 4d82c1ef shell input touchscreen tap 733 417";
+                Process process;
+                try {
+                    process=Runtime.getRuntime().exec(cmd);
+                    System.out.println(InputStream2String(process.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        },1000,10000,TimeUnit.MILLISECONDS);
+    }
+
+
+
+
+
+
+    public static String InputStream2String(InputStream inputStream){
+
+
+
+        String result="";
+        BufferedReader br=new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String temp="";
+            while ((temp=br.readLine())!=null){
+                result+=temp+"\n";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
