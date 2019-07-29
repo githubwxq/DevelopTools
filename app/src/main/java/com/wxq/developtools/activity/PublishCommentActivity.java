1c4d1543c7a5b926e601590d10ecb45ba6ec41b1
王晓清 <805380422@qq.com>
2019-07-29 13:23:00 +0800

    评论多图上传

--- app/src/main/java/com/wxq/developtools/activity/PublishCommentActivity.java
+++ app/src/main/java/com/wxq/developtools/activity/PublishCommentActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import com.juziwl.uilibrary.edittext.SuperEditText;
 import com.luck.picture.lib.PictureSelectorView;
+import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
 import com.luck.picture.lib.utils.DisplayUtils;
 import com.qiniu.android.http.ResponseInfo;
@@ -26,7 +27,6 @@ import com.wxq.developtools.model.SaveCommentParmer;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.reactivestreams.Publisher;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -36,7 +36,6 @@ import butterknife.BindView;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
-import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.functions.Function;
 
 import static com.wxq.commonlibrary.constant.GlobalContent.SEVENCLOUDDIR;
@@ -65,6 +64,8 @@ public class PublishCommentActivity extends BaseActivity {
         });
         productId = getIntent().getStringExtra("productId");
         picSelectView.initData(this, 3, 9, DisplayUtils.getScreenWidth(this) - DensityUtil.dip2px(this, 40));
+        picSelectView.setSelectModel(PictureMimeType.ofImage());
+
     }
 
     List<LocalMedia> selectList; //选中的图片集合
@@ -74,58 +75,42 @@ public class PublishCommentActivity extends BaseActivity {
 
     private void publishComment() {
         selectList = picSelectView.getSelectList();
-        ToastUtils.showShort(selectList.size() + "张图片被选中");
         if (StringUtils.isEmpty(etText.getText().toString())) {
             ToastUtils.showShort("请输入评论内容");
             return;
         }
-
         // 获取上传token
-        Flowable<String> tokenFlowable = Api.getInstance()
+        Api.getInstance()
                 .getApiService(KlookApi.class).getQiniuToken()
-                .compose(ResponseTransformer.handleResult());
-
-
-        tokenFlowable.flatMap(new Function<String, Flowable<List<String>>>() {
-            @Override
-            public Flowable<List<String>> apply(String token) throws Exception {
-
-                return Flowable.create(new FlowableOnSubscribe<List<String>>() {
+                .compose(ResponseTransformer.handleResult())
+                .flatMap(new Function<String, Flowable<List<String>>>() {
                     @Override
-                    public void subscribe(FlowableEmitter<List<String>> emitter) throws Exception {
-                        // 图片创建集合
-                        if (selectList.size() > 0) {
-                            needUpHash.clear();
-                            upLoadQiNiu(selectList.get(picNum), token,emitter);
-//                            emitter.onNext(needUpHash);
-                        } else {
-                            emitter.onNext(new ArrayList<>());
-                            emitter.onComplete();
-                        }
-
+                    public Flowable<List<String>> apply(String token) throws Exception {
+                        return Flowable.create(emitter -> {
+                            // 图片创建集合
+                            if (selectList.size() > 0) {
+                                needUpHash.clear();
+                                upLoadQiNiu(selectList.get(picNum), token, emitter);
+                            } else {
+                                emitter.onNext(new ArrayList<>());
+                                emitter.onComplete();
+                            }
+                        }, BackpressureStrategy.ERROR);
                     }
-                }, BackpressureStrategy.ERROR);
-            }
-        }).flatMap(new Function<List<String>, Publisher<?>>() {
-            @Override
-            public Publisher<?> apply(List<String> pics) throws Exception {
-                SaveCommentParmer saveCommentParmer=new SaveCommentParmer();
-                saveCommentParmer.content= etText.getText().toString();
-                saveCommentParmer.productId= productId;
-                saveCommentParmer.picUrls=pics;
-                return Api.getInstance().getApiService(KlookApi.class).saveComment(saveCommentParmer).compose(ResponseTransformer.handleResult());
-            }
-        })    .compose(RxTransformer.transformFlowWithLoading(this))
+                }).flatMap(pics -> {
+                    SaveCommentParmer saveCommentParmer = new SaveCommentParmer();
+                    saveCommentParmer.content = etText.getText().toString();
+                    saveCommentParmer.productId = productId;
+                    saveCommentParmer.picUrls = pics;
+                    return Api.getInstance().getApiService(KlookApi.class).saveComment(saveCommentParmer).compose(ResponseTransformer.handleResult());
+                }).compose(RxTransformer.transformFlowWithLoading(this))
                 .subscribe(new RxSubscriber<Object>() {
-
-            @Override
-            protected void onSuccess(Object o) {
-                ToastUtils.showShort("评论成功");
-                finish();
-            }
-        });
-
-
+                    @Override
+                    protected void onSuccess(Object o) {
+                        ToastUtils.showShort("评论成功");
+                        finish();
+                    }
+                });
     }
 
     private void upLoadQiNiu(LocalMedia localMedia, String token, FlowableEmitter<List<String>> emitter) {
