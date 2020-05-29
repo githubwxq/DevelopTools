ad3ee84f052332aabde76267347df885ea6a1bdf
wxq <805380422@qq.com>
2020-05-29 11:28:01 +0800

    kotlin mvvm代码提交

--- kotlintestdemo/src/main/java/com/example/kotlintestdemo/api/ApiService.kt
+++ kotlintestdemo/src/main/java/com/example/kotlintestdemo/api/ApiService.kt
@@ -4,16 +4,10 @@ import com.example.kotlintestdemo.bean.Article
 import retrofit2.http.*
 
 interface ApiService {
-
     companion object {
         const val BASE_URL = "https://www.wanandroid.com"
     }
-
-
     @GET("/article/top/json")
     suspend fun getTopArticleList(): ApiResult<List<Article>>
-
-
-
 
 }
